55e6e4047b527a085541712a17a76b8790f0f306
YanYasimo <yanmoura142529@gmail.com>
2022-03-26 00:25:49 -0300

    UserController and more API upgrades

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+module.exports = {
+    development: {
+        client: 'mysql',
+        connection: {
+            host : process.env.DB_HOST,
+            port : process.env.DB_PORT,
+            user : process.env.DB_USER,
+            password : process.env.DB_PASS,
+            database : process.env.DB_NAME
+        }
+    },
+    production: {
+        client: 'mysql',
+        connection: {
+            host : process.env.DB_HOST,
+            port : process.env.DB_PORT,
+            user : process.env.DB_USER,
+            password : process.env.DB_PASS,
+            database : process.env.DB_NAME
+        }
+    }
+};
