1e5421574d39f47a48f36a83d92ceb8fcc06a9cd
YanYasimo <yanmoura142529@gmail.com>
2022-05-05 11:32:37 -0300

    bcrypt session controller Login

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,15 +1,27 @@
 const connection = require('../database/connection');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async create (request, response){
         const { email, password } = request.body;
-        
-        const user = await connection('users').where('email', email).andWhere('password', password).select('id', 'name').first();
+        const user = await connection('users').select('*').where('email', email).first();
 
         if (!user) {
             return response.status(400).json({ "error": 'E-mail or password incorrects.' });
         }
 
-        return response.json(user);
+        const passwordIsValid = await bcrypt.compare(password, user.hashPassword);
+
+        if(!passwordIsValid) {
+            return response.status(400).json({ "error": 'E-mail or password incorrects.' });
+        }
+
+        return response.json(
+            {
+                "id" : user.id, 
+                "name" : user.name, 
+                "email" : user.email
+            }
+        );
     }
 }
